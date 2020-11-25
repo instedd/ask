6af5f7ebf7af059d91284fde7e060a2f10dfa28c
Mauricio <mmuller@manas.com.ar>
2020-11-24 22:40:50 -0300

    Add and dockerize Cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,43 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('log_into_instedd', (email, pwd) => {
+  cy.visit('https://login-stg.instedd.org/')
+
+  cy.get('.fieldset')
+    .find('input[name="user[email]"]')
+    .invoke('attr', 'value', email)
+    .should('have.value', email)
+
+  cy.get('.fieldset')
+    .find('input[name="user[password]"]')
+    .invoke('attr', 'value', pwd)
+    .should('have.value', pwd)
+
+  cy.contains('Log in').click()
+})
+
+import 'cypress-file-upload';
