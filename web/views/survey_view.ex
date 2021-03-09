56fbd88e54520d7e2c70473fcfaac45a17145f00
aeatencio <aeatencio@gmail.com>
2021-03-09 10:27:34 -0300

    WIP [skip ci] Panel surveys cards

--- web/views/survey_view.ex
+++ web/views/survey_view.ex
@@ -53,7 +53,9 @@ defmodule Ask.SurveyView do
       next_schedule_time: next_schedule_time(survey),
       updated_at: survey.updated_at,
       down_channels: survey.down_channels,
-      folder_id: survey.folder_id
+      folder_id: survey.folder_id,
+      is_panel_survey: Survey.panel_survey?(survey),
+      latest_panel_survey: survey.latest_panel_survey
     }
   end
   def render("survey_detail.json", %{survey: survey}) do
